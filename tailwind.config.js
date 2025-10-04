7ce866999f0dab404e17aa80779ae50b68e39648
Morshed10 <mokbd344@gmail.com>
2025-10-04 21:10:05 +0600

    bank-apps

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.{*}" // যদি আপনি JS/React ব্যাবহার করেন
+  ],
+
+extend: {
+  colors: {
+    dark1: "#191919",
+    dark2: "#1c1c1c"
+  }
+}
+}
